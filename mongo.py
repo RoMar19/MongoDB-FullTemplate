9fadf1928eb105fab104afbe32786dc545fa4d6f
Romart <rmf1312@hotmail.com>
2020-04-23 09:54:00 +0000

    Setting up environment variables

--- mongo.py
+++ mongo.py
@@ -1,5 +1,11 @@
 import pymongo
 import os
+from os import path
+if path.exists("env.py"):
+    import env
+
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+app.config['MONGODB_NAME'] = os.environ.get('MONGODB_NAME')
 
 MONGODB_URI = os.environ.get('MONGO_URI')
 DBS_NAME = "myTestDB"
