037afc81c012d76c22417e41e493005238c8860a
Romart <rmf1312@hotmail.com>
2020-04-23 10:09:32 +0000

    App.py deleted

--- app.py
+++
@@ -1,6 +0,0 @@
-import osfrom os import path
-if path.exists("env.py"):
-    import env
-
-app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
-app.config['MONGODB_NAME'] = os.environ.get('MONGODB_NAME')
